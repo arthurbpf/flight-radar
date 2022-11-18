3bda520ac5965219161883a9375193f801abf861
Arthur Fernandes <arthurbpfernandes@gmail.com>
2022-11-18 17:56:52 -0300

    feat: implement rotation function

--- src/utils/transformationFunctions.ts
+++ src/utils/transformationFunctions.ts
@@ -14,3 +14,24 @@ export function translate(airplane: Airplane, x: number, y: number): Airplane {
 
 	return airplane;
 }
+
+export function rotate(
+	airplane: Airplane,
+	rotationAngle: number,
+	xCenter: number,
+	yCenter: number
+): Airplane {
+	const x =
+		airplane.x * Math.cos(rotationAngle) - airplane.y * Math.sin(rotationAngle);
+	const y =
+		airplane.x * Math.sin(rotationAngle) + airplane.y * Math.cos(rotationAngle);
+
+	const { angle, radius } = convertToPolar(x, y);
+
+	airplane.x = x;
+	airplane.y = y;
+	airplane.angle = angle;
+	airplane.radius = radius;
+
+	return airplane;
+}
