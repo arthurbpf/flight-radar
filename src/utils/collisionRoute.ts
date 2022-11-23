2d5128db8ff96e89d36c8aae39f8322eaa539ed5
João Vitor Silva de Bitencourt <joaos_vb@hotmail.com>
2022-11-23 20:45:50 -0300

    ajuste calculo de colisão

--- src/utils/collisionRoute.ts
+++ src/utils/collisionRoute.ts
@@ -21,6 +21,7 @@ interface ReturnGetTimeDistance {
 }
 
 export function collisionRoute(time: number) {
+	debugger;
 	const airplanesRiskOfCollision = <AirplanesRiskOfCollision[]>[];
 
 	const collisionPoints = <CollisionPoint[]>[];
@@ -56,23 +57,23 @@ export function collisionRoute(time: number) {
 				});
 			}
 		}
-
-		airplanesRiskOfCollision.sort((itemA, itemB) =>
-			itemA.timeToCollision < itemB.timeToCollision ? -1 : 1
-		);
-
-		airplanesRiskOfCollision.forEach((item) => {
-			logs.push(
-				`Avião (X: ${item.airplaneA.x}; Y: ${
-					item.airplaneA.y
-				}) T: ${item.timeToPointA.toFixed(2)}s | Avião (X: ${
-					item.airplaneB.x
-				}; Y: ${item.airplaneB.y}) T: ${item.timeToPointB.toFixed(
-					2
-				)}s - Colisão em: ${item.timeToCollision.toFixed(2)}s`
-			);
-		});
 	});
+
+	airplanesRiskOfCollision.sort((itemA, itemB) =>
+	itemA.timeToCollision < itemB.timeToCollision ? -1 : 1
+);
+
+airplanesRiskOfCollision.forEach((item) => {
+	logs.push(
+		`Avião (X: ${item.airplaneA.x}; Y: ${
+			item.airplaneA.y
+		}) T: ${item.timeToPointA.toFixed(2)}s | Avião (X: ${
+			item.airplaneB.x
+		}; Y: ${item.airplaneB.y}) T: ${item.timeToPointB.toFixed(
+			2
+		)}s - Colisão em: ${item.timeToCollision.toFixed(2)}s`
+	);
+});
 
 	setStateCollision({ collisionPoint: collisionPoints });
 
@@ -185,6 +186,51 @@ function handle90and270cases(
 	airplaneB: Airplane,
 	directionBInRad: number
 ): ReturnGetTimeDistance {
+	if(airplaneB.direction === 0 ||
+		airplaneB.direction === 180||
+		airplaneB.direction === 360){
+			let riskOfCollision = true;
+			debugger;
+
+	const dA = Number(
+			Math.sqrt(
+				Math.pow(0, 2) + Math.pow(Math.abs(airplaneB.y - airplaneA.y), 2)
+			).toFixed(4)
+		),
+		dB = Number(
+			Math.sqrt(
+				Math.pow(airplaneA.x - airplaneB.x, 2) +
+					Math.pow(0, 2)
+			).toFixed(4)
+		);
+
+	const tA = (dA / airplaneA.speed) * 3600,
+		tB = (dB / airplaneB.speed) * 3600;
+
+	const timeToCollision = Math.abs(tA - tB);
+
+	return {
+		riskOfCollision,
+		timeToCollision,
+		timeToPointA: tA,
+		timeToPointB: tB,
+		collisionPoint: {
+			x: airplaneA.x,
+			y: airplaneB.y
+		}
+	};	
+		} else if(airplaneB.direction === 90 || airplaneB.direction === 270) {
+			return {
+				riskOfCollision: false,
+				timeToCollision: 0,
+				timeToPointA: 0,
+				timeToPointB: 0,
+				collisionPoint: {
+					x: 0,
+					y: 0
+				}
+			}
+		}
 	const coefB = Number(Math.tan(directionBInRad).toFixed(2)) || 0;
 
 	const yFinalB = coefB * -airplaneB.x + airplaneB.y,
